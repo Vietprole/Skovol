52ba3ecce1d35687cbe9c4b2d947643102f24064
truc-nt <truc.ntw@gmail.com>
2023-08-27 03:47:46 +0700

    v1

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,35 +1,50 @@
 import { ethers } from 'ethers';
 import { useState } from 'react';
-import { BrowserProvider } from "ethers";
 import './App.css';
 
 function App() {
-  let signer = null
+  const abi = require('./abi.json')
+  const contractAddress = "0xd9145CCE52D386f254917e481eB44e9943F39138"
   const [walletAddress, setWalletAddress] = useState("")
 
-  const requestAccount = async () => {
-    console.log("request account")
+  const [provider, setProvider] = useState(null)
+  const [signer, setSigner] = useState(null)
+  const [contract, setContract] = useState(null)
 
+  const requestAccount = async () => {
     try {
       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      console.log(accounts)
       setWalletAddress(accounts)
     } catch (err) {
-      console.log(err)
-    }
-
-    if (window.ethereum) {
-      console.log("detected ethereum")
-    } else {
-      console.log("MetaMask not installed")
+      console.log("cuu")
+      alert("Need to install MetaMask!")
     }
   }
 
   const connectWallet = async () => {
     if (typeof window.ethereum !== 'undefined') {
       await requestAccount()
-      const provider = new ethers.BrowserProvider(window.ethereum)
-      signer = await provider.getSigner();
+      updateEthers()
+    } else {
+      alert("Need to install MetaMask!")
     }
+  }
+
+  const updateEthers = async () => {
+    const tempProvider = new ethers.BrowserProvider('ws://127.0.0.1:9944')
+    setProvider(tempProvider)
+    const tempSigner = await tempProvider.getSigner()
+    setSigner(tempSigner)
+    console.log(tempProvider, tempSigner)
+    const contract = new ethers.Contract(contractAddress, abi, tempSigner)
+    setContract(contract)
+  }
+
+  const getVal = async () => {
+    const val = await contract.dataOf('019')
+    console.log(val)
+
   }
 
   return (
@@ -37,6 +52,7 @@ function App() {
       <header className="App-header">
         <button onClick={connectWallet}>Connect Wallet</button>
         <p>{walletAddress}</p>
+        <button onClick={getVal}>Get Value</button>
       </header>
     </div>
   );
